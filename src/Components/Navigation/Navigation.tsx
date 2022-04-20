7cf86e380478d5601928ce0c7b1f8f2891ba889a
kashmir1 <lzlin@mail.ru>
2022-04-20 15:32:02 +1000

    show/hide menu

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,20 +1,16 @@
 import React, {useState} from "react";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Container, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import './navigation.css'
-import set = Reflect.set;
 
 function Navigation(): JSX.Element {
 
     const [isMenuOpen, setIsMenuOpen] = useState<Boolean>(false);
-    const [isNavbarOpen, setIsNavbarOpen] = useState<Boolean>(false)
 
     const menuToggle = () => {
         setIsMenuOpen(!isMenuOpen)
     }
 
-    // @ts-ignore
-    // @ts-ignore
     return (
         <React.Fragment>
             <div className="container">
@@ -24,10 +20,9 @@ function Navigation(): JSX.Element {
                             <div className='col-lg-6 position-relative'>
                               <div className={!isMenuOpen ? 'toggle-open' : 'toggle-close'} onClick={menuToggle}></div>
                             </div>
-
                             <div className='col-lg-6'><Navbar.Brand href="#home">Галерея</Navbar.Brand></div>
                         </div>
-                        <nav>
+                        <nav className={!isMenuOpen ? 'd-none' : 'd-block'}>
                             <ul className="list-unstyled">
                                 <li>
                                     <NavLink className={({isActive}) => (isActive ? 'active' : '')} to="/">Галерея</NavLink>
