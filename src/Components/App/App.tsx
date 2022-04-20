2164f72bad73e0d1266d9892b8db74da472adc5e
kashmir1 <lzlin@mail.ru>
2022-04-20 15:48:48 +1000

    create Gallery-list, Gallery-item component. Render gallery item

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,24 +2,14 @@ import React, {useState} from 'react';
 import './App.css';
 import {Button, Card, Container, Navbar, NavDropdown} from 'react-bootstrap';
 import Navigation from "../Navigation/Navigation";
+import GalleryList from "../Gallery-list/Gallery-list";
 
 function App() {
 
     return (
     <div className="App">
         <Navigation />
-
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+        <GalleryList />
     </div>
   );
 }
